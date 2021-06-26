b658124439a6eabfe17fb60bd3456cee65b3b78b
deller <scoobydooplayer@gmx.de>
2021-06-27 00:16:50 +0200

    added create student dialog

--- client/user-service/src/app/components/manage-students/components/create-student/create-student.component.ts
+++ client/user-service/src/app/components/manage-students/components/create-student/create-student.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Gender } from '@common/dto/gender.enum';
+import { StudentDTO } from '@common/dto/student.dto';
 
 @Component({
   selector: 'create-student',
@@ -8,7 +10,18 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class CreateStudentComponent implements OnInit {
 
-  
+  public newStudent:StudentDTO|null={
+    active:true,
+    birthdate:new Date(),
+    firstname:"",
+    gender:Gender.MALE,
+    lastname:"",
+    mail:"",
+    matriculationNumber:"",
+    phone:"",
+    studyCourse:null,
+    title:""
+  };
   constructor(
     public dialogRef: MatDialogRef<CreateStudentComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData
