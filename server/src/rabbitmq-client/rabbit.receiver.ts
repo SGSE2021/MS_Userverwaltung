2a82e7803d3d478e8a8606d2db1f9edb92f7c5e0
deller <scoobydooplayer@gmx.de>
2021-07-06 17:08:00 +0200

    finsihed rabbitmq

--- server/src/rabbitmq-client/rabbit.receiver.ts
+++ server/src/rabbitmq-client/rabbit.receiver.ts
@@ -1,34 +1,33 @@
 
 import * as amqp from "amqplib/callback_api"
 
-export class RabbitReceiver{
-    constructor(queue:string,handler:(queue:string,message:string) => void){
+export class RabbitReceiver {
+    constructor( queue: string, handler: ( queue: string, message: string ) => void ) {
         const url = process.env.RABBIT_MQ || 'amqp://guest:guest@localhost:5672/'
-        amqp.connect(url, function(error0, connection) {
-    if (error0) {
-        console.log(error0)
-        //throw error0;
-    }
+        amqp.connect( url, function ( error0, connection ) {
+            if ( error0 ) {
+                console.log( error0 )
+            }
 
-    connection.createChannel(function(error1, channel) {
-        if (error1) {
-            console.log("Rabbit Receiver:",error1)
-        }
+            connection.createChannel( function ( error1, channel ) {
+                if ( error1 ) {
+                    console.log( "Rabbit Receiver:", error1 )
+                }
 
-        channel.assertQueue(queue, {
-            durable: false
-        });
+                channel.assertQueue( queue, {
+                    durable: false
+                } );
 
-        console.log(`Waiting for messages in ${queue}`);
+                console.log( `Waiting for messages in ${ queue }` );
 
-        channel.consume(queue, function(msg) {
-            console.log(`Rabbit Receiver: ${msg?.content.toString()}`);
- 
-            //handler(queue,msg);
-        }, {
-            noAck: true
-        });
-    });
-});
+                channel.consume( queue, function ( msg ) {
+                    console.log( `Rabbit Receiver: ${ msg?.content.toString() }` );
+
+                    //handler(queue,msg?.content);
+                }, {
+                    noAck: true
+                } );
+            } );
+        } );
     }
 }
