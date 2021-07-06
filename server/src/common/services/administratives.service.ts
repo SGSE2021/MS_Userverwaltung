6b687923fa7338728c6a42f15fffe4406992da7a
deller <scoobydooplayer@gmx.de>
2021-07-06 16:02:11 +0200

    refactor

--- server/src/common/services/administratives.service.ts
+++ server/src/common/services/administratives.service.ts
@@ -2,14 +2,16 @@ import prisma from "../../database";
 
 export class AdministrativeService {
     public async getAllAdministratives() {
-        const foundAdministratives = await prisma.administrative.findMany();       
+        const foundAdministratives = await prisma.administrative.findMany();
         return foundAdministratives;
     }
 
-    public async getAdministrativeById(id:string) {
-        const foundAdministrative = await prisma.administrative.findFirst({where:{
-            id:id
-        }});
+    public async getAdministrativeById( id: string ) {
+        const foundAdministrative = await prisma.administrative.findFirst( {
+            where: {
+                id: id
+            }
+        } );
         return foundAdministrative;
-    }   
+    }
 }
