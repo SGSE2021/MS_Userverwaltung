46f089e45af83d6b98a694da5e89d2fbcbbd8208
deller <scoobydooplayer@gmx.de>
2021-07-06 09:38:15 +0200

    real first test

--- server/tests/integration.test.ts
+++ server/tests/integration.test.ts
@@ -1,17 +1,18 @@
-// import {StudentsRoute} from "../src/rest-server-client/routes/students.route"
-// import {RestServer}from "../src/server"
-// import request from "supertest"
+import {StudentsRoute} from "../src/rest-server-client/routes/students.route"
+import {RestServer}from "../src/server"
+import request from "supertest"
 
-// const testServer = new RestServer([new StudentsRoute()]);
+const testServer = new RestServer([new StudentsRoute()]);
 
 describe("Test API",()=>{
 
-    describe("GET /movies",()=>{
+    const studentsRoute ="/students";
+    describe(`GET ${studentsRoute}`,()=>{
 
-    it("should return all movies on GET /movies",async ()=>{  
-        // const test = await request(testServer.app).get("/students");
-        // expect(test.statusCode).toEqual(200);
-        expect(true).toBe(true); 
+    it(`should return all Students on GET ${studentsRoute}`,async ()=>{  
+        const test = await request(testServer.app).get(studentsRoute);
+        expect(test.statusCode).toEqual(200);
+        console.log(test.body)
         })
 
 
