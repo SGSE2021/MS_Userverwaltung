4fea3b39a7e28836b7fafdd062fcfbd8131ca198
deller <scoobydooplayer@gmx.de>
2021-07-07 18:39:39 +0200

    changed frontend api

--- client/user-service/src/environments/environment.prod.ts
+++ client/user-service/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  restApi: 'https://' + 'sgse2021.westeurope.cloudapp.azure.com/users-api'
+  restApi: 'https://' + 'sgse2021.westeurope.cloudapp.azure.com/api/users'
 };
