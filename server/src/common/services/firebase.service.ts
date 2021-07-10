d627046b68c3f72311df338636395668bcdaa681
deller <scoobydooplayer@gmx.de>
2021-07-10 23:26:13 +0200

    changed firebase sdk config

--- server/src/common/services/firebase.service.ts
+++ server/src/common/services/firebase.service.ts
@@ -9,15 +9,12 @@ const adminConfig = require("../../../../server/firebase-admin-config.json")
 const adminApp = firebaseAdmin.initializeApp( { credential: firebaseAdmin.credential.cert( JSON.parse( JSON.stringify( adminConfig ) ) ) } );
 export { adminApp };
 
-const config = require("../../../../server/firebase-admin-config")
+const config = require("../../../../server/firebase-config")
 
 const app = firebase.default.initializeApp( config );
 export { app };
 
 export async function sendEmailVerification(uid:string){
-
-
-
         const token = await adminApp.auth().createCustomToken(uid);
         const result = await app.auth().signInWithCustomToken(token);
         await result.user?.sendEmailVerification();
