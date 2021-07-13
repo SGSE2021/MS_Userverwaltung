3bdf48d149341e1f9eecdc6cec2096b3d414de43
deller <scoobydooplayer@gmx.de>
2021-07-13 13:19:50 +0200

    revert

--- server/src/common/controllers/auth.controller.ts
+++ server/src/common/controllers/auth.controller.ts
@@ -10,13 +10,11 @@ export class AuthController {
     public getUserInfo = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
         try {
             const token:string = req.params.token;
-            console.log("AuthController:GetUserInfo: token from client",token);
+            console.log(token);
             const userInfo = await this.authService.getUserInfo(token);
             if(!userInfo){
-                console.log("AuthController:GetUserInfo: User info sent to client", userInfo);
                 res.status(404);
             }
-            console.log("AuthController:GetUserInfo: User info sent to client", userInfo);
             res.status(200).json(userInfo);
         } catch (error) {
             next(error);
